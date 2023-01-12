707e6d9a5d3770e9cfa2f19a5e8663d445ef4342
maduredes <maria.redes@windi.com.br>
2023-01-12 14:02:21 -0300

    subindo alterações rocket

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import { useState } from 'react'
-import {Post} from './Post'
+import { useState } from 'react';
+import { Header } from './components/Header';
+import {Post} from './Post';
+import styles from './App.module.css' ;
+import './global.css';
+import { SideBar } from './components/Sidebar';
 export function App() {
 
   return (
-    <Post author="Maria"
+    <div>
+      <Header/>
+      <div className={styles.wrapper}>
+        <SideBar/>
+        <main>  
+            <Post author="Maria"
     content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias similique ipsum repudiandae aut facilis nam quos modi libero ipsa nihil. Accusantium architecto qui aliquid ullam? Dolorem provident aliquam tenetur recusandae.
-"/>
+      "/></main>
+      </div>
+
+</div>
   )
 }
 
